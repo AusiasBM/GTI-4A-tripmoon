5d74e252d071f4be23dd049c74e45aa99267c182
Aitor <aibes1001@gmail.com>
2022-12-04 19:06:47 +0100

    Escena trailer con video 360

---
+++ trip-moon/Assets/VideoController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public GameObject video;
+    double duration = 0f;
+    double time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = video.GetComponent<VideoPlayer>().length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time > duration) SceneManager.LoadScene("Interior_Bunker");
+    }
+}
