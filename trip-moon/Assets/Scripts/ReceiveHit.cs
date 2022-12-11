237338d3710f3f499767a6a51b229997d9ec2a55
Ausias BM <ausiasbm@gmail.com>
2022-12-11 19:40:53 +0100

    botones configurados

---
+++ trip-moon/Assets/Scripts/ReceiveHit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ReceiveHit : MonoBehaviour
+{
+    public UnityEvent myEvent;
+    
+    public void OnPointerEnter()
+    {
+    }
+
+    public void OnPointerExit()
+    {
+    }
+
+    public void OnPointerClick()
+    {
+        myEvent.Invoke();
+    }
+}
